88ca42d1208098bed1b1dcabfb4aff428e0d8d95
man0n0n0 <camporini@protonmail.com>
2025-03-14 18:01:57 +0100

    added a launchfile

--- software/ros_ws_delta/src/delta_ros/setup.py
+++ software/ros_ws_delta/src/delta_ros/setup.py
@@ -1,6 +1,9 @@
 from setuptools import find_packages, setup
+import os 
+from glob import glob 
 
 package_name = 'delta_ros'
+package_name = 'depth_processing_launch'
 
 setup(
     name=package_name,
@@ -10,6 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
