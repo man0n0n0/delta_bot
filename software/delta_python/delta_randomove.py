c1cb1dd7a8a4ced884a8471013e4222bd1f41590
camporini@protonmail.com <manoah.camporini@erg.school>
2025-03-22 11:52:34 +0100

    encoding

--- software/delta_python/delta_randomove.py
+++ software/delta_python/delta_randomove.py
@@ -18,16 +18,16 @@ sleep(3)
 s.flushInput()  # Flush startup text in serial input
 
 '''send homing command'''
-s.write('G28\n')
+s.write(b"G28\n".format(seq))
 grbl_out = s.read_until(b'ok\n')
-'''gcode line processing'''
 
+'''gcode line processing'''
 try : 
 	Xl = np.random.randint(-X, X + 1)
 	Yl = np.random.randint(-Y, Y)
 	Zl = np.random.randint(Z[0],Z[1])
 	Fl = np.random.randint(F_min,F_max)
-	s.write("G1 X{Xl} Y{Yl} Z{Zl} F{Fl}\n")
+	s.write(b"G1 X{Xl} Y{Yl} Z{Zl} F{Fl}\n")
 
 	grbl_out = s.read_until(b'ok\n')
 	#grbl_out = s.readline() # Wait for response with carriage return
