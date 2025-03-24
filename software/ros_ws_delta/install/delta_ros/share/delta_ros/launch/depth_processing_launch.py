94fa27ce60357ca6e157bea2bd4ff534bb7f4483
man0n0n0 <camporini@protonmail.com>
2025-03-24 15:04:44 +0100

    ros2 ws implementation inclued in a launchfile

--- software/ros_ws_delta/install/delta_ros/share/delta_ros/launch/depth_processing_launch.py
+++ software/ros_ws_delta/install/delta_ros/share/delta_ros/launch/depth_processing_launch.py
@@ -5,16 +5,13 @@ def generate_launch_description():
     return launch.LaunchDescription([
         launch_ros.actions.Node(
             package='grbl_ros',
-            executable='grbl_node',
-            name='gcode sender'),
+            executable='grbl_node'),
 
         launch_ros.actions.Node(
-        package='arducam_rclpy_tof_pointcloud',
-        executable='tof_pointcloud',
-        name='arducamTOF activation'),
+            package='arducam_rclpy_tof_pointcloud',
+            executable='tof_pointcloud'),
         
         launch_ros.actions.Node(
-        package='delta_ros',
-        executable='effector_to_closest',
-        name='open cv process'),
+            package='delta_ros',
+            executable='effector_to_closest'),
   ])
