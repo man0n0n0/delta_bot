30c1474d59b4488ed3c918945708f8ecee5c75d8
man0n0n0 <camporini@protonmail.com>
2025-03-25 19:51:34 +0100

    g0z0

--- software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
@@ -34,7 +34,9 @@ class EffectorToClosestPointCloudNode(Node):
             self.pointcloud_callback,
             10)
 
-      
+        time.sleep(5)
+        self.send_grbl_command(f"G0Z0")
+        
         # Last processing time to control the rate
         self.last_process_time = self.get_clock().now()
         
