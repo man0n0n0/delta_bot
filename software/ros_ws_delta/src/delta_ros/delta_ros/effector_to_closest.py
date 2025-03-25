5d6a2d95d1a256aa0e46578c0600c6c4ec4361bc
man0n0n0 <camporini@protonmail.com>
2025-03-25 19:01:07 +0100

    gcode msg

--- software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
@@ -118,7 +118,7 @@ class EffectorToClosestPointCloudNode(Node):
             z_machine = average_point[2] * 1000.0  # Convert to mm
             
             # Send movement command
-            self.send_grbl_command(f"$G1X{x_machine:.0f}Y{y_machine:.0f}Z{z_machine:.0f}F1000")
+            self.send_grbl_command(f"$G1X{x_machine:.0f}Y{y_machine:.0f}Z-{z_machine:.0f}F1000")
             self.get_logger().info("go to the point")
             
             # Wait a moment to ensure movement is complete
