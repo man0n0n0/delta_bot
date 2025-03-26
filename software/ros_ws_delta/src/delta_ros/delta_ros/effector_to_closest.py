7a742896fc62978b4f80e4f7f075fcff4636f71a
man0n0n0 <camporini@protonmail.com>
2025-03-26 10:42:26 +0100

    time gestion (marlin)

--- software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
@@ -37,8 +37,8 @@ class EffectorToClosestPointCloudNode(Node):
         # Last processing time to control the rate
         self.last_process_time = self.get_clock().now()
         
-        # Create a timer to send reset command after a short delay
-        self.create_timer(1.0, self.send_reset_command)
+        # Store the timer reference
+        self.reset_timer = self.create_timer(1.0, self.send_reset_command)
         
         self.get_logger().info("Effector to closest point cloud node initialized!")
 
@@ -46,7 +46,9 @@ class EffectorToClosestPointCloudNode(Node):
         """Send reset command and stop the timer"""
         self.send_grbl_command("$G0X0Y0Z0")
         # Destroy the timer after sending the command
-        self.destroy_timer(self.timers[0])
+        if self.reset_timer:
+            self.destroy_timer(self.reset_timer)
+            self.reset_timer = None
         self.get_logger().info("Reset command sent to effector")
 
     def send_grbl_command(self, command):
