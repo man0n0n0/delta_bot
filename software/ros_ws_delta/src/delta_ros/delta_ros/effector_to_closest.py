34fc849f330c765a14f04f629ccf502f1bc69416
man0n0n0 <camporini@protonmail.com>
2025-03-26 11:27:33 +0100

    z opposite

--- software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/effector_to_closest.py
@@ -114,7 +114,7 @@ class EffectorToClosestPointCloudNode(Node):
             
             # Convert point coordinates to machine coordinates (mm)
             x_machine = average_point[0] * 1000.0 # Convert to mm 
-            y_machine = average_point[1] * 1000.0  # Convert to mm 
+            y_machine = average_point[1] * 1000.0 * -1 # Convert to mm and opposite number
             #TODO: create a function to correct lens distortion for small distances
             z_machine = (average_point[2] * 1000.0) + 50  # Convert to mm and add a correction 
             
