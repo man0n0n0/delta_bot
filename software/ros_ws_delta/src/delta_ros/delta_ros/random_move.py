f3c5326e7f141a030fca70172d5c6fe83a4a2d0d
man0n0n0 <camporini@protonmail.com>
2025-03-28 10:11:43 +0100

    faster 4hz

--- software/ros_ws_delta/src/delta_ros/delta_ros/random_move.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/random_move.py
@@ -11,7 +11,7 @@ class EffectorRandomMovementNode(Node):
         
         # Declare parameters
         self.declare_parameter('grbl_action_name', '/delta_marlin/send_gcode_cmd')
-        self.declare_parameter('processing_rate', 2.0)  # How often to move (Hz)
+        self.declare_parameter('processing_rate', 4.0)  # How often to move (Hz)
         
         # Get parameters
         self.grbl_action_name = self.get_parameter('grbl_action_name').value
