b7a24862ecc41b173454320ca121108337df5213
man0n0n0 <camporini@protonmail.com>
2025-04-02 19:28:00 +0200

    home snapshot and move logic + slower rate

--- software/ros_ws_delta/src/delta_ros/delta_ros/over_closest.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/over_closest.py
@@ -17,7 +17,7 @@ class EffectorToClosestPointCloudNode(Node):
         self.declare_parameter('pointcloud_topic', '/point_cloud')
         self.declare_parameter('grbl_action_name', '/delta_marlin/send_gcode_cmd')
         self.declare_parameter('min_distance_threshold', 0.005)  # Minimum distance in meters to trigger action
-        self.declare_parameter('processing_rate', 1.0)  # How often to process point clouds (Hz)
+        self.declare_parameter('processing_rate', 0.25)  # How often to process point clouds (Hz)
         self.declare_parameter('num_closest_points', 300)  # Number of closest points to average
         self.declare_parameter('x_limit', )  # Number of closest points to average
 
@@ -64,54 +64,44 @@ class EffectorToClosestPointCloudNode(Node):
         
 
     def pointcloud_callback(self, pointcloud_msg):
+        '''go home / take snapshot / go 10mm over closest point'''
+        
+        self.send_grbl_command("$G28")
+        time.sleep(1)
+
         # Convert point cloud to numpy array with explicit coordinate extraction
         points_list = []
         for point in pc2.read_points(pointcloud_msg, skip_nans=True):
-            # Mirror the x-coordinate (change the sign)
-            # This effectively flips the point cloud to adapt 
-            # y x order inverted  
             points_list.append((point[1], point[0], point[2]))
-        
-        # Log point cloud information
+
         total_points = len(points_list)
                     
-        # Check if point cloud is empty
         if total_points == 0:
             self.get_logger().warn("Empty point cloud received")
             return
         
-        # Convert to numpy array of floats
         points_array = np.array(points_list, dtype=np.float64)
         
-        # Calculate distances from origin
         distances = np.linalg.norm(points_array, axis=1)
 
-        # Sort points by distance
         sorted_indices = np.argsort(distances)
         
         # Take the n closest points
         closest_points_indices = sorted_indices[:self.num_closest_points]
         closest_points = points_array[closest_points_indices]
         
-        # Calculate the average of the closest points
         average_point = np.mean(closest_points, axis=0)
         
-        # Calculate average distance of the closest points
-        # average_distance = np.mean(distances[closest_points_indices])
-
         self.get_logger().info(f"{average_point}")
 
-        # Convert point coordinates to machine coordinates (mm)
-        x_machine = (self.prev_average_point[0] + average_point[0])* 1000.0 # Convert to mm 
-        y_machine = (self.prev_average_point[1] + average_point[1]) * 1000.0 # Convert to mm 
-        z_machine = 110
+        x_machine = (average_point[0])* 1000.0 # Convert to mm 
+        y_machine = (average_point[1]) * 1000.0 # Convert to mm 
+        z_machine = 10 + (average_point[2]* 1000.0) # Convert to mm // take to acocunt that atm the machine z0 isnt touching the plate
 
         self.prev_average_point = average_point 
 
-        # Print the closest points information
         self.get_logger().info(f"Average point coordinates: X={x_machine:.0f}, Y={y_machine:.0f}, Z={z_machine:.0f}")
         
-        # Send movement command
         self.send_grbl_command(f"$G1X{x_machine:.0f}Y{y_machine:.0f}Z{z_machine:.0f}F6000")
 
 
