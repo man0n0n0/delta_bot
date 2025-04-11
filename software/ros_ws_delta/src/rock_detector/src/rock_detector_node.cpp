119509d2902d0e1373249d50c2213fdf91d357dc
man0n0n0 <camporini@protonmail.com>
2025-04-11 13:33:11 +0200

    more tolerance, ut adjusted nb of points

--- software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
+++ software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
@@ -82,9 +82,9 @@ private:
     // Cluster extraction (to separate individual rocks)
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-    ec.setClusterTolerance(0.005);  // how close points need to be to each other to be considered part of the same cluster. 
-    ec.setMinClusterSize(1000);     // Minimum points in a cluster
-    ec.setMaxClusterSize(25000);   // Maximum points in a cluster
+    ec.setClusterTolerance(0.01);  // how close points need to be to each other to be considered part of the same cluster. 
+    ec.setMinClusterSize(2000);     // Minimum points in a cluster
+    ec.setMaxClusterSize(20000);   // Maximum points in a cluster
     ec.setInputCloud(objects);
     
     // Create KdTree for search
