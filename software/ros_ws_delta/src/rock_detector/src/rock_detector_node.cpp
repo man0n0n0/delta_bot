6489dbb04a3ed47af0f367ce1e1ec4f68218b49c
man0n0n0 <camporini@protonmail.com>
2025-04-11 14:42:44 +0200

    tolerance to 10mm

--- software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
+++ software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
@@ -88,7 +88,7 @@ private:
     // Cluster extraction (to separate individual rocks)
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
-    ec.setClusterTolerance(0.005);  // how close points need to be to each other to be considered part of the same cluster. 
+    ec.setClusterTolerance(0.01);  // how close points need to be to each other to be considered part of the same cluster. 
     ec.setMinClusterSize(50);     // Minimum points in a cluster
     ec.setMaxClusterSize(20000);   // Maximum points in a cluster
     ec.setInputCloud(objects);
