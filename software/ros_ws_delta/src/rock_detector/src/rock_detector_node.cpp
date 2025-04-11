308dec92bc831cc372552958d9e727a1f2357276
man0n0n0 <camporini@protonmail.com>
2025-04-11 13:42:51 +0200

    table threshold smller /2 AND smaller clustre

--- software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
+++ software/ros_ws_delta/src/rock_detector/src/rock_detector_node.cpp
@@ -52,7 +52,7 @@ private:
     seg.setOptimizeCoefficients(true);
     seg.setModelType(pcl::SACMODEL_PLANE);
     seg.setMethodType(pcl::SAC_RANSAC);
-    seg.setDistanceThreshold(0.01);  // threshold from above the table 
+    seg.setDistanceThreshold(0.005);  // threshold from above the table 
     seg.setInputCloud(cloud_filtered);
     seg.segment(*table_inliers, *coefficients);
 
@@ -83,7 +83,7 @@ private:
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<pcl::PointXYZ> ec;
     ec.setClusterTolerance(0.01);  // how close points need to be to each other to be considered part of the same cluster. 
-    ec.setMinClusterSize(1000);     // Minimum points in a cluster
+    ec.setMinClusterSize(500);     // Minimum points in a cluster
     ec.setMaxClusterSize(20000);   // Maximum points in a cluster
     ec.setInputCloud(objects);
     
