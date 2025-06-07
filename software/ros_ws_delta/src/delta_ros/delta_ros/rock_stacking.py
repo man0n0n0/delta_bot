38e1eee71974aabc573abba4590dfeb5e66296b6
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-07 19:12:15 +0200

    absolute rock pos

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -46,6 +46,9 @@ class RockStacking(Node):
         rock_x = rock.position.x * 1000  # Convert to mm
         rock_y = rock.position.y * 1000
         rock_z = rock.position.z * 1000
+
+        #init cairn height to zero
+        cairn_height =0
         
         self.get_logger().info(f'Processing rock at ({rock_x:.1f}, {rock_y:.1f}, {rock_z:.1f})')
         
@@ -53,12 +56,22 @@ class RockStacking(Node):
         self.send_gcode('M8')  # Open gripper
         self.send_gcode('G0 Z150 F1000')  # Move to safe height
         self.send_gcode(f'G0 X{rock_x:.1f} Y{rock_y:.1f}')  # Move above rock
-        self.send_gcode(f'G1 Z{rock_z + 10:.1f} F500')  # Move down to pick
+
+        self.send_gcode(f'G91')  # relative positioning
+        self.send_gcode(f'G1 Z-{rock_z - 10:.1f} F500')  # Move down to pick
+        self.send_gcode(f'G90')  # absolute positioning
+
         self.send_gcode('M9')  # Close gripper
         time.sleep(5)  # Wait for grip
         
         self.send_gcode('G1 Z200 F1000')  # Lift rock
         self.send_gcode('G0 X0 Y0')  # Move to center
+
+        self.send_gcode(f'G91')  # relative positioning
+        self.send_gcode(f'G1 Z-{rock_z - 10:.1f} F500')  # Move down to drop (here implement the last_rock pos for cairn making)
+        cairn_height += rock_z 
+        self.send_gcode(f'G90')  # absolute positioning
+
         self.send_gcode('G1 Z110 F500')  # Move down to place
         self.send_gcode('M8')  # Open gripper
         self.send_gcode('G0 Z200')  # Move up
