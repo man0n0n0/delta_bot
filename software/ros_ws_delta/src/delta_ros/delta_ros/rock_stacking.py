49706c55e0680bc11fc8b97450cc1a2e637347f6
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-07 21:25:17 +0200

    height correction

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -72,7 +72,7 @@ class RockStacking(Node):
         time.sleep(10)  # Wait for grip
         
         self.send_gcode(f'G91')  # relative positioning
-        self.send_gcode(f'G1 Z{rock_z:.1f} F500')  # Lift rock
+        self.send_gcode(f'G1 Z{rock_z - safe_movement_height - height_correction - tool_offset[2]:.1f} F500')  # Lift rock
         cairn_height += rock_z 
         self.send_gcode(f'G90')  # absolute positioning
 
