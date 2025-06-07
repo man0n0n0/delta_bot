35bb9daa624d8086880a6824958f3b65383c17f1
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-07 18:37:51 +0200

    timing control

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -23,7 +23,7 @@ class RockStacking(Node):
         # Send home command at startup
         time.sleep(1) # wait for marlin warming
         self.send_gcode('G28')
-        time.sleep(5)
+        time.sleep(10)
         
         self.get_logger().info('Rock Stacking node started')
     
@@ -51,18 +51,17 @@ class RockStacking(Node):
         
         # Pick and place sequence
         self.send_gcode('M8')  # Open gripper
-        self.send_gcode('G0 Z200 F1000')  # Move to safe height
+        self.send_gcode('G0 Z150 F1000')  # Move to safe height
         self.send_gcode(f'G0 X{rock_x:.1f} Y{rock_y:.1f}')  # Move above rock
         self.send_gcode(f'G1 Z{rock_z + 10:.1f} F500')  # Move down to pick
         self.send_gcode('M9')  # Close gripper
         time.sleep(2)  # Wait for grip
         
-        self.send_gcode('G0 Z200')  # Lift rock
+        self.send_gcode('G1 Z200 F1000')  # Lift rock
         self.send_gcode('G0 X0 Y0')  # Move to center
         self.send_gcode('G1 Z110 F500')  # Move down to place
         self.send_gcode('M8')  # Open gripper
         self.send_gcode('G0 Z200')  # Move up
-        self.send_gcode('G28')  # Return home
         
         self.get_logger().info('Rock stacking completed')
 
