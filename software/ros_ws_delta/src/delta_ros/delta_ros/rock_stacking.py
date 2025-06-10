df572d18666f02bb5d8d70a776bc7580a0968f2a
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-10 16:40:47 +0200

    relative correction

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -105,6 +105,7 @@ class RockStacking(Node):
 
         # Correct the rock placement
         self.get_logger().info('Correction the rock placement : moving to newly mesured')
+        self.send_gcode('G91')
         self.send_gcode(f'G1 X{self.rock_x+self.tool_offset[0]:.1f} Y{self.rock_y+self.tool_offset[1]:.1f} F2000')
 
         # Complete picking sequence
