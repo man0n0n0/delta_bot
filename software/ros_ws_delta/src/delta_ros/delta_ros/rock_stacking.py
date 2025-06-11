2edb05eddb457b8d6c3d71cb144b34c6a05adf65
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-11 13:35:02 +0200

    timing

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -42,7 +42,7 @@ class RockStacking(Node):
         # Constants
         self.safe_height = 150
         self.height_correction = 0
-        self.tool_offset = (0, -35, 5)
+        self.tool_offset = (0, 35, 5)
         
         # Debug: Log subscription info
         self.get_logger().info('Rock Stacking node initializing...')
@@ -54,7 +54,7 @@ class RockStacking(Node):
         # Send home command at startup
         time.sleep(5)
         self.send_gcode('G28')
-        time.sleep(10)
+        time.sleep(20)
         self.get_logger().info('Rock Stacking node started')
 
     def debug_callback(self):
