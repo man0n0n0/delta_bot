a0c22c1da287f26f47d16340e1329a5450feb501
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 17:05:03 +0200

    1.2 approch coeff

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -43,7 +43,7 @@ class RockStacking(Node):
         self.safe_height = 100
         self.height_correction = 0
         self.tool_offset = (0, -55, -10)
-        self.approch_coeff = -1
+        self.approch_coeff = 1,2
 
         # Debug: Log subscription info
         self.get_logger().info('Rock Stacking node initializing...')
