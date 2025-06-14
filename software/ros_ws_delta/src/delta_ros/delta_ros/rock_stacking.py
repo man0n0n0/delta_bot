47f30f0f1ad46cf0437ef29f0cabb067c2a37097
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 23:55:15 +0200

    ok

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -44,7 +44,7 @@ class RockStacking(Node):
         self.safe_height = 100
         self.height_correction = 0
         self.tool_offset = (0, -35, 10)
-        self.approch_coeff = 1.1
+        self.approch_coeff = 1
         self.unloaded_speed = 5000
 
         # Debug: Log subscription info
