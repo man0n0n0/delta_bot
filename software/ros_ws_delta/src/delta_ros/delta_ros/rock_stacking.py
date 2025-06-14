c5a078c6461b7d7e8416828a79af5599f0b70bf9
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 22:38:08 +0200

    fine tuning

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -114,7 +114,7 @@ class RockStacking(Node):
         self.placing_y = placing_rock.position.y * 1000
         self.placing_z = placing_rock.position.z * 1000
 
-        rock = msg.poses[1]  
+        rock = msg.poses[len(msg.poses)//2-1]  # take the median point spot
         self.rock_x = rock.position.x * 1000
         self.rock_y = rock.position.y * 1000
         self.rock_z = rock.position.z * 1000
