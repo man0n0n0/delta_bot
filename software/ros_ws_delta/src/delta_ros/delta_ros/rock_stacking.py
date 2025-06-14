c2eb1f4ff2bffc24dc70ae1d6477eaeb9c2d7257
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 17:54:16 +0200

    pyhthon logic

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -133,7 +133,8 @@ class RockStacking(Node):
         self.get_logger().info('Starting Stage 2: Getting closer to the rock')
         self.stage = 'stage2'
 
-        rock = msg.poses[len(msg.poses)] # Second measure for rock picking (suppose that we pick the smallest is the pool)
+        index = len(msg.poses)
+        rock = msg.poses[index] # Second measure for rock picking (suppose that we pick the smallest is the pool)
         self.rock_x = rock.position.x * 1000
         self.rock_y = rock.position.y * 1000
         self.rock_z = rock.position.z * 1000
@@ -145,7 +146,9 @@ class RockStacking(Node):
         time.sleep(5)
 
     def stage3_callback(self, msg):
-        rock = msg.poses[len(msg.poses)]  # Second measure for rock picking 
+        index = len(msg.poses)
+        rock = msg.poses[index] # third measure for rock picking (suppose that we pick the smallest is the pool)
+
         self.rock_x = rock.position.x * 1000
         self.rock_y = rock.position.y * 1000
         self.rock_z = rock.position.z * 1000
