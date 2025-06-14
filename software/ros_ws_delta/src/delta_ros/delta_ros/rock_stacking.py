a2fe76df291d35b7b61340132947e87770781786
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 16:55:47 +0200

    tool offset

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -42,7 +42,7 @@ class RockStacking(Node):
         # Constants
         self.safe_height = 100
         self.height_correction = 0
-        self.tool_offset = (0, -45, -10)
+        self.tool_offset = (0, 45, -10)
         self.approch_coeff = 1
         
         # Debug: Log subscription info
