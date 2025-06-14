ca373f27d8b50d8948e50cd2ec5845c95b519802
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-14 23:01:26 +0200

    hah

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -144,7 +144,7 @@ class RockStacking(Node):
         # Correct the rock placement
         self.get_logger().info('Correction the rock placement : moving to newly measured')
         self.send_gcode('G91')
-        self.rock_second_approch = self.rock_z * 0.3
+        self.rock_second_approch = self.rock_z * 0.1
         self.send_gcode(f'G1 X{self.rock_x*self.approch_coeff:.1f} Y{(self.rock_y*self.approch_coeff):.1f} Z-{self.rock_second_approch} F{self.unloaded_speed}')
         time.sleep(5)
 
