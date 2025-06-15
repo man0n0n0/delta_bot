06705ac1beae18c849335436d93df8343c5625b5
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-15 14:53:23 +0200

    smalelr coeef index

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -44,7 +44,7 @@ class RockStacking(Node):
         self.safe_height = 100
         self.height_correction = 0
         self.tool_offset = (0, -45,-20)
-        self.approch_coeff = 1.4
+        self.approch_coeff = 1.3
         self.unloaded_speed = 5000
 
         # Debug: Log subscription info
