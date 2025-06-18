13234c21a6cc7b9decd4799ca3fb8a218ebb2124
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-18 17:53:10 +0200

    toward the qualite

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -139,7 +139,7 @@ class RockStacking(Node):
         self.placing_y = placing_rock.position.y * 1000
         self.placing_z = placing_rock.position.z * 1000
 
-        rock = msg.poses[int(len(msg.poses))-1]  # go toward the smallest
+        rock = msg.poses[int(len(msg.poses)*0.75)-1]  # go toward the smallest
         self.rock_x = rock.position.x * 1000
         self.rock_y = rock.position.y * 1000
         self.rock_z = rock.position.z * 1000
