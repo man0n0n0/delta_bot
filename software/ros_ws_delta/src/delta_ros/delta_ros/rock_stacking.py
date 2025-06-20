2bec9d464055b117c3f6de16152913083cddfa12
camporini@protonmail.com <manoah.camporini@erg.school>
2025-06-20 11:40:19 +0200

    no stage intermediate

--- software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
+++ software/ros_ws_delta/src/delta_ros/delta_ros/rock_stacking.py
@@ -130,17 +130,17 @@ class RockStacking(Node):
 
             self.stage1_callback(msg)
 
-        # # Stage 2: intermediate rock compensation
-        # elif self.stage == 'stage1':
-        #     self.stage2_callback(msg)
+        # Stage 2: intermediate rock compensation
+        elif self.stage == 'stage1':
+            self.stage2_callback(msg)
 
         # Stage 3: Get updated rock_z and complete placement
-        elif self.stage == 'stage1':
+        elif self.stage == 'stage2':
             self.stage3_callback(msg)
 
     def stage1_callback(self, msg):
         self.get_logger().info('Starting Stage 1: Getting placement values and first approch')
-        self.stage = 'stage1'
+        self.stage = 'stage2'
         self.operation_in_progress = True
         
         # Get rock positions
